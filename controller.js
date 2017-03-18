b7c58c4a04330ba1fcb8e335036fe8f12a2b54f1
Spencer Simons <spencersimons@Spencers-MacBook-Pro.local>
2017-03-18 12:39:28 -0600

    all finished

---
+++ controller.js
@@ -0,0 +1,9 @@
+module.exports = {
+    getPlanes: function(app){
+        var db = app.get('db');
+
+        db.get_planes([25],function(err, planes){
+            console.log(err, planes);
+        })
+    }
+}
--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var massive = require('massive');
+var controller = require('./controller');
+
+
+var connectionString = "postgres://spencersimons@localhost/sandbox";
+var massiveInstance = massive.connectSync({connectionString : connectionString});
+
 
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.set('db',massiveInstance);
+
+var db = app.get('db');
+
+controller.getPlanes(app);
 
 app.listen('3000', function(){
-  console.log("Successfully listening on : 3000")	
+  console.log("Successfully listening on : 3000")
 })
-
